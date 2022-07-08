2908578b56975f6ceedd2ccff4f8a6e6587bcbe2
Emmanuel Aranda <emmanuelaranda27@gmail.com>
2022-07-08 13:01:54 -0500

    valid anagram

--- validAnagram/valid-anagram.js
+++ validAnagram/valid-anagram.js
@@ -1,11 +1,16 @@
 var isAnagram = function (s, t) {
     let hitCount = 0;
+    let tempString = Array.from(t);
     for (let i = 0; i < s.length; i++) {
-        if (t.includes(s[i])) {
+        let indexOfChar = tempString.indexOf(s[i]);
+        console.log(s[i] + ' in position ' + indexOfChar);
+        if (indexOfChar >= 0) {
+            tempString.splice(indexOfChar, 1);
             hitCount += 1;
         }
     }
-    return hitCount === s.length;
+    console.log('hit count is ' + hitCount + ' ; s.length is ' + s.length);
+    return hitCount === s.length && s.length === t.length;
 }
 
 module.exports = isAnagram;
--- validAnagram/valid-anagram.test.js
+++ validAnagram/valid-anagram.test.js
@@ -8,3 +8,15 @@ test('rat, car : false', () => {
     expect(isAnagram('rat', 'car')).toBe(false);
 })
 
+test('a, ab : false', () => {
+    expect(isAnagram('a', 'ab')).toBe(false);
+})
+
+test('aa, a : false', () => {
+    expect(isAnagram('aa', 'a')).toBe(false);
+})
+
+test('aacc, ccac : false', () => {
+    expect(isAnagram('aacc', 'ccac')).toBe(false);
+})
+
